b4717dbb0f1ffadc2729ac50805530cf0b7a0031
Ping Xu <nickxucn@hotmail.com>
2023-11-06 15:23:40 -0500

    upload assignment 1

--- src/app.py
+++ src/app.py
@@ -1,63 +1,34 @@
-import os
-import time
-import azure.cognitiveservices.speech as speechsdk
+def sample_abstractive_summarization(document) -> None:
+    # [START abstract_summary]
+    import os
+    from azure.core.credentials import AzureKeyCredential
+    from azure.ai.textanalytics import TextAnalyticsClient
 
-def conversation_transcriber_recognition_canceled_cb(evt: speechsdk.SessionEventArgs):
-    print('Canceled event')
+    endpoint = os.environ["AZURE_LANGUAGE_ENDPOINT"]
+    key = os.environ["AZURE_LANGUAGE_KEY"]
 
-def conversation_transcriber_session_stopped_cb(evt: speechsdk.SessionEventArgs):
-    print('SessionStopped event')
+    text_analytics_client = TextAnalyticsClient(
+        endpoint=endpoint,
+        credential=AzureKeyCredential(key),
+    )
 
-def conversation_transcriber_transcribed_cb(evt: speechsdk.SpeechRecognitionEventArgs):
-    print('TRANSCRIBED:')
-    if evt.result.reason == speechsdk.ResultReason.RecognizedSpeech:
-        print('\tText={}'.format(evt.result.text))
-        print('\tSpeaker ID={}'.format(evt.result.speaker_id))
-    elif evt.result.reason == speechsdk.ResultReason.NoMatch:
-        print('\tNOMATCH: Speech could not be TRANSCRIBED: {}'.format(evt.result.no_match_details))
+    poller = text_analytics_client.begin_abstract_summary(document)
+    abstract_summary_results = poller.result()
+    for result in abstract_summary_results:
+        if result.kind == "AbstractiveSummarization":
+            print("Summaries abstracted:")
+            [print(f"{summary.text}\n") for summary in result.summaries]
+        elif result.is_error is True:
+            print("...Is an error with code '{}' and message '{}'".format(
+                result.error.code, result.error.message
+            ))
+    # [END abstract_summary]
 
-def conversation_transcriber_session_started_cb(evt: speechsdk.SessionEventArgs):
-    print('SessionStarted event')
 
-def recognize_from_file():
-    # This example requires environment variables named "SPEECH_KEY" and "SPEECH_REGION"
-    speech_config = speechsdk.SpeechConfig(subscription=os.environ.get('AZURE_SPEECH_KEY'), region=os.environ.get('AZURE_SPEECH_REGION'))
-    speech_config.speech_recognition_language="en-US"
-    
-    audio_file_path = "c:\\Repository\\azure\\azureai\\src\\BillGates_2010.wav" 
-    # audio_file_path = "C:\\Repository\\azure\\ai-900\\data\\speech\\yearwiththesaints_00_anonymous.wav" 
-    # audio_file_path = "C:\\Repository\\azure\\Week1\\speechService.wav"
-    audio_config = speechsdk.audio.AudioConfig(filename=audio_file_path)
-    conversation_transcriber = speechsdk.transcription.ConversationTranscriber(speech_config=speech_config, audio_config=audio_config)
-
-    transcribing_stop = False
-
-    def stop_cb(evt: speechsdk.SessionEventArgs):
-        #"""callback that signals to stop continuous recognition upon receiving an event `evt`"""
-        print('CLOSING on {}'.format(evt))
-        nonlocal transcribing_stop
-        transcribing_stop = True
-
-    # Connect callbacks to the events fired by the conversation transcriber
-    conversation_transcriber.transcribed.connect(conversation_transcriber_transcribed_cb)
-    conversation_transcriber.session_started.connect(conversation_transcriber_session_started_cb)
-    conversation_transcriber.session_stopped.connect(conversation_transcriber_session_stopped_cb)
-    conversation_transcriber.canceled.connect(conversation_transcriber_recognition_canceled_cb)
-    # stop transcribing on either session stopped or canceled events
-    conversation_transcriber.session_stopped.connect(stop_cb)
-    conversation_transcriber.canceled.connect(stop_cb)
-
-    conversation_transcriber.start_transcribing_async()
-
-    # Waits for completion.
-    while not transcribing_stop:
-        time.sleep(.5)
-
-    conversation_transcriber.stop_transcribing_async()
-
-# Main
-
-try:
-    recognize_from_file()
-except Exception as err:
-    print("Encountered exception. {}".format(err))
+if __name__ == "__main__":
+    audio_file_path = "C:\\Repository\\azure\\azureai\\data\\speech\\yearwiththesaints_00_anonymous.txt" 
+    with open(audio_file_path, 'r') as file:
+        lines = [line.strip('\n') for line in file.readlines()]
+    separator = ', '
+    result = [separator.join(lines)]
+    sample_abstractive_summarization(result)
