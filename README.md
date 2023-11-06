8de7c4eb43e3d7a8e128428ddefa2bbfe33a70d8
Ping Xu <nickxucn@hotmail.com>
2023-11-05 20:27:31 -0500

    add new files

--- README.md
+++ README.md
@@ -28,6 +28,14 @@ C. How to upload VS code to Github,
 2. Using Git with Visual Studio Code \
    https://www.youtube.com/watch?v=i_23KUAEtUM
 
+## Switch Python Interpreter
+To work with Python in Jupyter Notebooks, you must activate an Anaconda environment in VS Code, or another Python environment in which you've installed the Jupyter package. To select an environment, use the <strong>Python: Select Interpreter</strong> command from the Command Palette (Ctrl+Shift+P).
+
+Once the appropriate environment is activated, you can create and open a Jupyter Notebook, connect to a remote Jupyter server for running code cells, and export a Jupyter Notebook as a Python file.
+
+## Create or open a Jupyter Notebook
+You can create a Jupyter Notebook by running the <strong>Create: New Jupyter Notebook</strong> command from the Command Palette (Ctrl+Shift+P) or by creating a new .ipynb file in your workspace.
+
 ## Access to Azure OpenAI
 
 1.  Register Domain and email in [GoDaddy](https://www.godaddy.com/en-ca), first year is around C$42
--- src/app.py
+++ src/app.py
@@ -1,1 +1,63 @@
-print("Hello World!")
+import os
+import time
+import azure.cognitiveservices.speech as speechsdk
+
+def conversation_transcriber_recognition_canceled_cb(evt: speechsdk.SessionEventArgs):
+    print('Canceled event')
+
+def conversation_transcriber_session_stopped_cb(evt: speechsdk.SessionEventArgs):
+    print('SessionStopped event')
+
+def conversation_transcriber_transcribed_cb(evt: speechsdk.SpeechRecognitionEventArgs):
+    print('TRANSCRIBED:')
+    if evt.result.reason == speechsdk.ResultReason.RecognizedSpeech:
+        print('\tText={}'.format(evt.result.text))
+        print('\tSpeaker ID={}'.format(evt.result.speaker_id))
+    elif evt.result.reason == speechsdk.ResultReason.NoMatch:
+        print('\tNOMATCH: Speech could not be TRANSCRIBED: {}'.format(evt.result.no_match_details))
+
+def conversation_transcriber_session_started_cb(evt: speechsdk.SessionEventArgs):
+    print('SessionStarted event')
+
+def recognize_from_file():
+    # This example requires environment variables named "SPEECH_KEY" and "SPEECH_REGION"
+    speech_config = speechsdk.SpeechConfig(subscription=os.environ.get('AZURE_SPEECH_KEY'), region=os.environ.get('AZURE_SPEECH_REGION'))
+    speech_config.speech_recognition_language="en-US"
+    
+    audio_file_path = "c:\\Repository\\azure\\azureai\\src\\BillGates_2010.wav" 
+    # audio_file_path = "C:\\Repository\\azure\\ai-900\\data\\speech\\yearwiththesaints_00_anonymous.wav" 
+    # audio_file_path = "C:\\Repository\\azure\\Week1\\speechService.wav"
+    audio_config = speechsdk.audio.AudioConfig(filename=audio_file_path)
+    conversation_transcriber = speechsdk.transcription.ConversationTranscriber(speech_config=speech_config, audio_config=audio_config)
+
+    transcribing_stop = False
+
+    def stop_cb(evt: speechsdk.SessionEventArgs):
+        #"""callback that signals to stop continuous recognition upon receiving an event `evt`"""
+        print('CLOSING on {}'.format(evt))
+        nonlocal transcribing_stop
+        transcribing_stop = True
+
+    # Connect callbacks to the events fired by the conversation transcriber
+    conversation_transcriber.transcribed.connect(conversation_transcriber_transcribed_cb)
+    conversation_transcriber.session_started.connect(conversation_transcriber_session_started_cb)
+    conversation_transcriber.session_stopped.connect(conversation_transcriber_session_stopped_cb)
+    conversation_transcriber.canceled.connect(conversation_transcriber_recognition_canceled_cb)
+    # stop transcribing on either session stopped or canceled events
+    conversation_transcriber.session_stopped.connect(stop_cb)
+    conversation_transcriber.canceled.connect(stop_cb)
+
+    conversation_transcriber.start_transcribing_async()
+
+    # Waits for completion.
+    while not transcribing_stop:
+        time.sleep(.5)
+
+    conversation_transcriber.stop_transcribing_async()
+
+# Main
+
+try:
+    recognize_from_file()
+except Exception as err:
+    print("Encountered exception. {}".format(err))
